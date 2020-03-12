b85c5aeb6a1a8b30a470c848006359ec5f2fe424
Priya Ravi <priyaravi23@gmail.com>
2020-03-11 18:17:28 -0700

    implemented react search component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import mockDataService from './components/mock-data-service';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        // contains the json
+        states: [],
+        filteredStates: [],
+        // What the user has entered
+        userInput: ""
+    };
+
+    constructor(props) {
+        super(props);
+        this.mockDataService = new mockDataService();
+    }
+
+    componentDidMount() {
+        this.getItems();
+    }
+
+    render() {
+        return (
+            <div>
+                <p>
+                    <input type="text"
+                           id="filter"
+                           placeholder="Search for..."
+                           onChange={this.handleChange}
+                    />
+                </p>
+
+                <ul>
+                    {this.state.filteredStates.map(item => (
+                        <li key={item.name}
+                            value={item.name}>
+                            {item.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+
+
+    getItems() {
+        this.mockDataService.retrieveItems().then(states => {
+                this.setState({
+                    states: states,
+                    filteredStates: [...states]}
+                );
+            }
+        );
+    }
+
+    // Event fired when the input value is changed
+    handleChange = (e) => {
+        const userInput = e.target.value;
+        const re = new RegExp(userInput, 'i');
+        // Filter our suggestions that don't contain the user's input
+        const filteredStates = this.state.states.filter((item, index) => {
+            return re.test(item.name);
+        });
+
+        this.setState({
+            filteredStates,
+            userInput: e.target.value
+        });
+    };
 }
 
 export default App;
