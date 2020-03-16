baf73f66f7fb752da49d36fc688b5337d91439eb
Priya Ravi <priyaravi23@gmail.com>
2020-03-16 15:44:10 -0700

    implemented search component using hooks

--- src/App.js
+++ src/App.js
@@ -1,72 +1,51 @@
-import React, {Component} from 'react';
-import mockDataService from './components/mock-data-service';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
-class App extends Component {
-    state = {
-        // contains the json
-        states: [],
-        filteredStates: [],
-        // What the user has entered
-        userInput: ""
+export default function App () {
+    const [users, setUsers] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState([]);
+
+    useEffect(() => {
+        axios('https://jsonplaceholder.typicode.com/users')
+            .then(res =>
+                res.data.map(item => ({
+                    name: `${item.name}`,
+                    username: `${item.username}`,
+                    email: `${item.email}`
+                }))
+            )
+            .then(data => {
+                setUsers(data);
+                setFilteredUsers(data);
+            })
+    }, []);
+
+    const handleInputChange = (e) => {
+      const input = e.target.value;
+      const re = new RegExp(input, 'i');
+
+      const filteredUsers = users.filter(item => {
+          return re.test(item.name)
+      });
+
+      setFilteredUsers(filteredUsers)
     };
 
-    constructor(props) {
-        super(props);
-        this.mockDataService = new mockDataService();
-    }
+    return (
+        <div>
+            <input type='text'
+                   autoFocus
+                   placeholder='Search for a User'
+                   onChange={handleInputChange} />
 
-    componentDidMount() {
-        this.getItems();
-    }
-
-    render() {
-        return (
-            <div>
-                <p>
-                    <input type="text"
-                           id="filter"
-                           placeholder="Search for..."
-                           onChange={this.handleChange}
-                    />
-                </p>
-
-                <ul>
-                    {this.state.filteredStates.map(item => (
-                        <li key={item.name}
-                            value={item.name}>
-                            {item.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        );
-    }
-
-
-    getItems() {
-        this.mockDataService.retrieveItems().then(states => {
-                this.setState({
-                    states: states,
-                    filteredStates: [...states]}
-                );
-            }
-        );
-    }
-
-    // Event fired when the input value is changed
-    handleChange = (e) => {
-        const userInput = e.target.value;
-        const re = new RegExp(userInput, 'i');
-        // Filter our suggestions that don't contain the user's input
-        const filteredStates = this.state.states.filter((item, index) => {
-            return re.test(item.name);
-        });
-
-        this.setState({
-            filteredStates,
-            userInput: e.target.value
-        });
-    };
+            <ul>
+                {filteredUsers.map(user => (
+                    <li key={user.name}
+                        value={user.name} >
+                        {user.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
-
-export default App;
---
+++ src/components/search.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import mockDataService from './mock-data-service';
+
+class Search extends Component {
+    state = {
+        // contains the json
+        states: [],
+        filteredStates: [],
+        // What the user has entered
+        userInput: ""
+    };
+
+    constructor(props) {
+        super(props);
+        this.mockDataService = new mockDataService();
+    }
+
+    componentDidMount() {
+        this.getItems();
+    }
+
+    getItems() {
+        this.mockDataService.retrieveItems().then(states => {
+                this.setState({
+                    states: states,
+                    filteredStates: [...states]}
+                );
+            }
+        );
+    }
+
+    // Event fired when the input value is changed
+    handleChange = (e) => {
+        const userInput = e.target.value;
+        const re = new RegExp(userInput, 'i');
+        // Filter our suggestions that don't contain the user's input
+        const filteredStates = this.state.states.filter((item, index) => {
+            return re.test(item.name);
+        });
+
+        this.setState({
+            filteredStates,
+            userInput: e.target.value
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <p>
+                    <input type="text"
+                           id="filter"
+                           placeholder="Search for..."
+                           onChange={this.handleChange}
+                    />
+                </p>
+
+                <ul>
+                    {this.state.filteredStates.map(item => (
+                        <li key={item.name}
+                            value={item.name}>
+                            {item.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Search;
